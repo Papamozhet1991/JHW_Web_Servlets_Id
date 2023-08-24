4f83119e9a0c2ba6e37befe6cc12b7b971191604
Artyom Skovorodko <papamozhet1991@yandex.ru>
2023-08-24 23:47:54 +0500

    Add files

---
+++ src/main/java/org/askovorodko/model/Post.java
@@ -0,0 +1,13 @@
+package org.askovorodko.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Post {
+    private long id;
+    private String content;
+}
